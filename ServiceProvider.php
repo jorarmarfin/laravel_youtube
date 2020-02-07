4810f45efec5a6d5ccc8c49a5f0a0ba309c66bb4
Luis Mayta <luis.mayta@gmail.com>
2020-02-07 16:38:34 -0500

    first commit

---
+++ ServiceProvider.php
@@ -0,0 +1,37 @@
+<?php namespace JorarMarfin\LaravelYoutube;
+
+use Illuminate\Support\Facades\App;
+use Illuminate\Foundation\AliasLoader;
+use Illuminate\Support\ServiceProvider;
+use JorarMarfin\MiPaquete\Controllers\MainController;
+
+class MiPaqueteServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        App::bind('MiPaquete', function()
+        {
+            return new MainController;
+        });
+        App::booting( function()
+        {
+            $loader = AliasLoader::getInstance();
+            $loader->alias('MiPaquete', 'JorarMarfin\MiPaquete\Facades\MiPaqueteFacade');
+        });
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
---
+++ src/Controllers/MainController.php
@@ -0,0 +1,13 @@
+<?php namespace JorarMarfin\LaravelYoutube\Controllers;
+
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class MainController extends Controller
+{
+    public function getData()
+    {
+        return "Hola mundo";
+    }
+}
